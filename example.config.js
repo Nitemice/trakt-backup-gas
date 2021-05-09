1d87e093107178c5668102f2478a775e67c146f9
Nitemice <webmaster@nitemice.net>
2021-05-09 17:30:47 +1000

    Added README & example config

---
+++ example.config.js
@@ -0,0 +1,7 @@
+const config = {
+    "username": "username",
+    "clientId": "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+    "clientSecret": "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb",
+
+    "backupDir": "xzshxhX8QjQnlNuF_KWzSytboL6EvtSLW"
+};
